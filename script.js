d0b02f23928268417e191aa9f142a3750de27d8e
Your Name <Oseremen Ojiefoh>
2022-03-23 14:00:57 -0500

    03/22

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function greeting() {
+function askName() {
   var name = prompt("What is your name?");
-
   return name
 }
 
+function greeting() {
+  alert(``)
+
+}
+
 function 
+
+alert(askName());

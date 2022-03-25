d9926b840b9afa0608cb74715662bc7bd493683c
Playbot <101743624+NotPlaybot@users.noreply.github.com>
2022-03-25 01:52:36 +0000

    we major?

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-var userTemp = 0;
-
 function askName() {
   var name = prompt("Welcome to Oseremen Ojiefoh's temperature converter! What is your name?");
   return name;
 }
 
-function askTemp(a) {
-  userTemp = prompt(`Hello, ${a}! What is the temperature?`);
+var name = askName(); 
+console.log(name);
+
+function askTemp(name) {
+  userTemp = prompt(`What's up, ${name}! What is the temperature?`);
   return userTemp;
 }
 
-var temp = askTemp(); 
+var temp = askTemp(name);
+console.log(temp);
 
-alert(askTemp(askName));
+function c2f(temp) {
+  var tempFarenheit = ((temp * 9) / 5) + 32;
+  alert(`${name}, ${temp}Cº is ${tempFarenheit}Fº.`);
+  
+  return tempFarenheit;
+}
 
-function askType(a) {
-  var tempType = prompt(`Hello, ${a}! How would you like to convert? For C to F enter 1. For F to C enter 2.`);
+function f2c(temp) {
+  var tempCelsius = ((temp - 32) * 5)/9;
+  alert(`${name}, ${temp}Fº is ${tempCelsius}Cº.`);
+  
+  return tempCelsius;
+}
 
-  if (tempType == 1) {
-    function c2f(temp) {
-      do {
-        temp = Number(prompt("What is the temperature in Celsius?"));
-      } while (isNaN(temp) == true)
+
+function askType(name) {
+  
+  var tempType = prompt(`Hello, ${name}! How would you like to convert? For C to F enter 1. For F to C enter 2.`);
     
-      var tempFarenheit = ((temp * 9) / 5) + 32;
-      alert(`${(askType(askName))}, ${temp}Cº is ${tempFarenheit}Fº.`);
-
-      return tempFarenheit;
-
-    }
+  if (tempType == 1) {
+    return c2f(temp);
   }
 
   if (tempType == 2) {
-    function f2c(temp) {
-      do {
-        temp = Number(prompt("What is the temperature in Farenheit?"));
-      } while (isNaN(temp) == true)
-    
-      var tempCelsius = ((temp - 32) * 5)/9;
-      alert(`${(askType(askName))}, ${temp}Fº is ${tempCelsius}Cº.`);
-
-      return tempCelsius;
-    }
+    return f2c(temp);
   }
+  
 }
+
+var ehitaleSux = askType(name);
+console.log(ehitaleSux);
